3e926dbe626e963e39f6f7784f100065537bde4f
jiwooH <jwwu9706@gmail.com>
2022-07-26 23:01:24 +0900

    first commit test

---
+++ bookingSystem/src/test/java/com/exam/bookingSystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.bookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
